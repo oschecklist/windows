9981f4ce587adf8f156592cf63e0253b9b8950e4
/Fox --develop <paul.liverman.iii@gmail.com>
2016-12-13 10:44:16 -0800

    Removed unneeded flag

--- list-programs.bat
+++ list-programs.bat
@@ -2,5 +2,5 @@
 REM Uses dir command to print directories of Program Files and Program Files (x86).
 REM Doesn't check for programs installed within subdirectories or anything like that.
 
-dir /w /b "C:\Program Files"
-dir /w /b "C:\Program Files (x86)"
+dir /b "C:\Program Files"
+dir /b "C:\Program Files (x86)"
