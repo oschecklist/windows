dfa2c23e42d2d9f3dcaea64c449900d71d3559b7
/Fox --develop <paul.liverman.iii@gmail.com>
2017-08-28 15:13:02 -0700

    fix scoop-setup
    still testing!

--- setup-scoop.ps1
+++ setup-scoop.ps1
@@ -2,20 +2,21 @@ Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
 
 # note: would prefer to install openssh and git separately, but those packages are broken at the time of writing
-scoop install nano git-with-openssh touch gcc busybox concfg 7zip curl wget cowsay dart figlet ffmpeg go grep kotlin tar lua pshazz r rsync rust sudo unrar unzip which zip processhacker whois python say
-
-concfg import solarized small   # make console look better and easier on the eyes
+scoop install git-with-openssh
 
 scoop bucket add extras
+
+scoop install nano touch gcc busybox concfg 7zip curl wget cowsay dart figlet ffmpeg go grep kotlin tar lua pshazz r rsync rust sudo unrar unzip which zip processhacker whois python say
+
+concfg import solarized small   # make console look better and easier on the eyes
 
 scoop bucket add lomeli-bucket https://github.com/Lomeli12/ScoopBucket
 scoop install hasher
 
-scoop bucket add g-bucket https://github.com/Guard13007/ScoopBucket
-scoop install rosus
+#scoop bucket add g-bucket https://github.com/Guard13007/ScoopBucket
+#scoop install rosus
 
 scoop bucket add g3-bucket https://github.com/Guard13007/ScoopBucket-ThirdParty
-
 scoop install moonscript love kitty
 
 scoop bucket add versions
