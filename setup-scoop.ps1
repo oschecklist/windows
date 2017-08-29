733a62fa37c5d512e99e986097a10f95e21d50c1
/Fox --develop <paul.liverman.iii@gmail.com>
2017-08-28 19:54:07 -0700

    Update setup-scoop.ps1

--- setup-scoop.ps1
+++ setup-scoop.ps1
@@ -16,13 +16,13 @@ scoop install hasher
 #scoop bucket add g-bucket https://github.com/Guard13007/ScoopBucket
 #scoop install rosus
 
-scoop bucket add g3-bucket https://github.com/Guard13007/ScoopBucket-ThirdParty
-scoop install moonscript love kitty
+#scoop bucket add g3-bucket https://github.com/Guard13007/ScoopBucket-ThirdParty
+scoop install moonscript love kitty   # these are in extras now, so my bucket is not needed
 
 scoop bucket add versions
-scoop bucket add love-versions https://github.com/Guard13007/ScoopBucket-LoveVersions
+scoop install ruby19   # I'm installing this older version because PIA relies on it (if I play with ruby later on, I need to use scoop reset to switch between them)
 
+# these were merged into official versions bucket, so this bucket is deprecated
+#scoop bucket add love-versions https://github.com/Guard13007/ScoopBucket-LoveVersions
 scoop install love0.9.1 love0.9.2 love0.10.0 love0.10.1 love0.10.2
 scoop reset love   # fix love to point to latest version
-
-scoop install ruby19   # I'm installing this older version because PIA relies on it (if I play with ruby later on, I need to use scoop reset to switch between them)
