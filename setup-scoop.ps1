fe3a39c9d30e99fbd6034704ef567ca29f7a83e0
/Fox --develop <paul.liverman.iii@gmail.com>
2017-12-20 17:00:07 -0800

    Update setup-scoop.ps1

--- setup-scoop.ps1
+++ setup-scoop.ps1
@@ -5,7 +5,18 @@ scoop install git-with-openssh
 
 scoop bucket add extras
 
-scoop install nano touch gcc busybox concfg 7zip curl wget cowsay dart figlet ffmpeg go grep kotlin tar lua pshazz r rsync rust sudo unrar unzip which zip processhacker whois python say
+# CLI tools
+scoop install nano touch gcc busybox concfg curl wget ffmpeg grep pshazz rsync sudo which whois
+# Utilities
+scoop install 7zip tar unrar zip unzip processhacker kitty
+# Languages / Programming
+scoop install dart go kotlin lua r rust python moonscript
+# Social
+scoop install telegram
+# Gaming / Game Dev
+scoop install love
+# Silly
+scoop install cowsay figlet say
 
 concfg import solarized small   # make console look better and easier on the eyes
 
@@ -16,10 +27,11 @@ scoop install hasher
 #scoop install rosus
 
 #scoop bucket add g3-bucket https://github.com/Guard13007/ScoopBucket-ThirdParty
-scoop install moonscript love kitty   # these are in extras now, so my bucket is not needed
+#scoop install moonscript love kitty   # these are in extras now, so my bucket is not needed
 
 scoop bucket add versions
-scoop install ruby19   # I'm installing this older version because PIA relies on it (if I play with ruby later on, I need to use scoop reset to switch between them)
+
+#scoop install ruby19   # I'm installing this older version because PIA relies on it (if I play with ruby later on, I need to use scoop reset to switch between them)
 
 # these were merged into official versions bucket, so this bucket is deprecated
 #scoop bucket add love-versions https://github.com/Guard13007/ScoopBucket-LoveVersions
